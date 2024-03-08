44c50b1e166176b4c1e9f82220eba0b04af920d7
Samuel Newhouse <samuel_newhouse@yahoo.com>
2024-03-07 18:21:54 -0600

    Add draw_move function

--- main.py
+++ main.py
@@ -64,12 +64,33 @@ class Cell:
             line = Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
             self._win.draw_line(line, "black")
 
+    def draw_move(self, to_cell, undo=False):
+        self_center_x = (self._x1 + self._x2) / 2
+        self_center_y = (self._y1 + self._y2) / 2
+        to_cell_center_x = (to_cell._x1 + to_cell._x2) / 2
+        to_cell_center_y = (to_cell._y1 + to_cell._y2) / 2
+
+        line = Line(Point(self_center_x, self_center_y), Point(to_cell_center_x, to_cell_center_y))
+        if undo:
+            self._win.draw_line(line,"gray")
+        else:
+            self._win.draw_line(line,"red")
+
 def main():
     win = Window(800, 600)
     p1 = Point(20,20)
     p2 = Point(40,40)
+    p3 = Point(40,20)
+    p4 = Point(60,40)
+
     c1 = Cell(p1, p2, win)
+    c2 = Cell(p3, p4, win)
+    c1.has_bottom_wall = False
+    c2.has_top_wall = False
     c1.draw()
+    c2.draw()
+
+    c1.draw_move(c2)
 
     win.wait_for_close()
 
